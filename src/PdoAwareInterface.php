ec63171ca5d48cc27b3ffa85007ac0fa1b79ad01
TOBENTOch <info@tobento.ch>
2025-09-22 11:23:56 +0200

    initial commit

---
+++ src/PdoAwareInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+ 
+namespace Tobento\Service\Storage;
+
+use PDO;
+
+interface PdoAwareInterface
+{
+    /**
+     * Returns the pdo.
+     *
+     * @return PDO
+     */
+    public function pdo(): PDO;
+}
